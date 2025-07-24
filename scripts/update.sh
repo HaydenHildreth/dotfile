c3c79f02998336f9f96dfe1ed1446ae6546b25d8
Hayden Hildreth <36833433+HaydenHildreth@users.noreply.github.com>
2025-07-24 10:46:12 -0400

    Add documentation for aliasing this script
    In case you'd like to use a keyword/alias instead of running script from file name. This would also be useful for any other script as well.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+# To alias this as a command, do the following:
+
+# First, ensure it can be ran/has permissions to do its tasks. See below:
+# chmod +x update.sh
+
+# Secondly, you need to add it to the ~/.bashrc location (may also be ~/.bash_aliases file):
+# Ex.: nano ~/.bashrc => alias updatesys='bash /path/to/script/update.sh'
+# Obviously, replace the /path/to/script/ directory with the actual PATH location on system
+
+# Finally, reload your shell's configuration after updating ~/.bashrc or ~/.bash_aliases:
+# If added to ~/.bashrc, do below:
+# source ~/.bashrc
+# If added to ~/.bash_aliases, do below:
+# source ~/.bash_aliases
+
+# Now you'll be able to execute this via the "alias" you've made. In my example, it would be "updatesys"
+
 sudo dnf upgrade # Download system upgrades...
 sudo shutdown -r now # Restart system immediately
